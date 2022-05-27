38a00a76c10906642d116426d05494a985843189
なるみ <narumi@maicoin.com>
2022-05-28 01:53:06 +0800

    add test case

---
+++ tests/test_glassnode.py
@@ -0,0 +1,16 @@
+from glassnode import Glassnode
+
+
+def test_glassnode():
+    g = Glassnode.from_env()
+
+    s = g.get(
+        category='market',
+        metric='marketcap_usd',
+        asset='btc',
+        since='2022-01-01 00:00:00',
+        until='2022-03-01 00:00:00',
+        frequency_interval='24h',
+    )
+
+    assert len(s) > 0
