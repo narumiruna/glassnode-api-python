8425abfd4d6685d91bf497f8c15c059b8d38a2a6
narumi <toucans-cutouts0f@icloud.com>
2025-03-31 18:37:16 +0800

    migrate to uv

--- src/glassnodeapi/glassnode.py
+++ src/glassnodeapi/glassnode.py
@@ -2,12 +2,12 @@ import json
 import os
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Union
 
+import httpx
 import iso8601
 import pandas as pd
-import requests
-from requests.utils import default_headers
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
 from .enums import Format
 from .enums import FrequencyInterval
@@ -17,17 +17,17 @@ ENDPOINT = "https://api.glassnode.com"
 
 
 def flatten_options(d: dict) -> dict:
-    options = d.get('o')
+    options = d.get("o")
     if options:
         d.update(options)
-        del d['o']
+        del d["o"]
     return d
 
 
 def convert_to_dataframe(data) -> pd.DataFrame:
     df = pd.DataFrame(data)
-    df['t'] = pd.to_datetime(df['t'], unit='s')
-    df = df.sort_values('t')
+    df["t"] = pd.to_datetime(df["t"], unit="s")
+    df = df.sort_values("t")
     return df
 
 
@@ -43,63 +43,63 @@ class Parameters:
     timestamp_format: TimestampFormat = None
 
     def to_dict(self) -> dict:
-        params = {'a': self.asset}
+        params = {"a": self.asset}
 
         if self.since is not None:
-            params['s'] = int(self.since.timestamp())
+            params["s"] = int(self.since.timestamp())
 
         if self.until is not None:
-            params['u'] = int(self.until.timestamp())
+            params["u"] = int(self.until.timestamp())
 
         if self.frequency_interval is not None:
-            params['i'] = self.frequency_interval.value
+            params["i"] = self.frequency_interval.value
 
         if self.format is not None:
-            params['f'] = self.format.value
+            params["f"] = self.format.value
 
         if self.currency is not None:
-            params['c'] = self.currency
+            params["c"] = self.currency
 
         if self.exchange is not None:
-            params['e'] = self.exchange
+            params["e"] = self.exchange
 
         if self.timestamp_format is not None:
-            params['timestamp_format'] = self.timestamp_format.value
+            params["timestamp_format"] = self.timestamp_format.value
 
         return params
 
 
-class Glassnode(object):
-
+class Glassnode:
     def __init__(self, api_key: str) -> None:
-        self.headers = default_headers()
+        self.headers = {}
 
         # attach API key
-        self.headers['X-Api-Key'] = api_key
+        self.headers["X-Api-Key"] = api_key
 
     def build_url(self, category: str, metric: str):
-        return os.path.join(ENDPOINT, 'v1', 'metrics', category, metric)
+        return os.path.join(ENDPOINT, "v1", "metrics", category, metric)
 
     def _get(self, category: str, metric: str, params: Parameters) -> dict:
         url = self.build_url(category, metric)
 
-        res = requests.get(url, params=params.to_dict(), headers=self.headers)
+        res = httpx.get(url, params=params.to_dict(), headers=self.headers)
         res.raise_for_status()
 
         return json.loads(res.text)
 
-    def get(self,
-            category: str,
-            metric: str,
-            asset: str,
-            since: Union[str, datetime] = None,
-            until: Union[str, datetime] = None,
-            frequency_interval: str = None,
-            format: str = None,
-            currency: str = None,
-            exchange: str = None,
-            timestamp_format: str = None) -> dict:
-
+    def get(
+        self,
+        category: str,
+        metric: str,
+        asset: str,
+        since: str | datetime = None,
+        until: str | datetime = None,
+        frequency_interval: str = None,
+        format: str = None,
+        currency: str = None,
+        exchange: str = None,
+        timestamp_format: str = None,
+    ) -> dict:
         if isinstance(since, str):
             since = iso8601.parse_date(since)
 
@@ -115,18 +115,21 @@ class Glassnode(object):
         if timestamp_format is not None:
             timestamp_format = TimestampFormat(timestamp_format)
 
-        params = Parameters(asset=asset,
-                            since=since,
-                            until=until,
-                            frequency_interval=frequency_interval,
-                            format=format,
-                            currency=currency,
-                            exchange=exchange,
-                            timestamp_format=timestamp_format)
+        params = Parameters(
+            asset=asset,
+            since=since,
+            until=until,
+            frequency_interval=frequency_interval,
+            format=format,
+            currency=currency,
+            exchange=exchange,
+            timestamp_format=timestamp_format,
+        )
 
         return self._get(category, metric, params)
 
     @classmethod
     def from_env(cls):
+        load_dotenv(find_dotenv())
         api_key = os.environ.get("GLASSNODE_API_KEY")
         return cls(api_key)
